67f57c060d1865089f3563d66ea56c2a837e67d6
r4f4elSR <rafaelsilva999897@gmail.com>
2025-03-23 17:38:38 -0300

    Quartz sync: Mar 23, 2025, 5:38 PM

---
+++ content/Untitled.md
@@ -0,0 +1,8 @@
+---
+title: Untitled
+draft: false
+tags:
+  - example-tag
+---
+ 
+Just a test. The rest of your content lives here. You can use **Markdown** here :)
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "The Natural Method",
+    pageTitle: "Quartz 4",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "r4f4elsr.github.io/the_natural_method/",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
